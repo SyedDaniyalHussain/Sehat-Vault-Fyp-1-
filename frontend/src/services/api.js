984744d8f11f56d7aa6fdd0bbf6d00d07aa54bd4
Syed Daniyal Hussain <your-email@example.com>
2026-08-20 01:51:21 +0500

    fix: update api url to render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,13 @@
+// import axios from 'axios';
+// const api=axios.create({baseURL:import.meta.env.VITE_API_URL||'http://localhost:5001/api',withCredentials:true});
+// export default api;
+
+
 import axios from 'axios';
-const api=axios.create({baseURL:import.meta.env.VITE_API_URL||'http://localhost:5001/api',withCredentials:true});
+
+const api = axios.create({
+  baseURL: 'https://sehat-vault-fyp-1.onrender.com/api',
+  withCredentials: true,
+});
+
 export default api;
