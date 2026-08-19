b5e775f7d2ac8aef2bf86eb0d96a7c024d67ea08
Syed Daniyal Hussain <your-email@example.com>
2026-08-20 02:39:20 +0500

    fix: add ssl support for cloud database

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,23 @@
+// import mysql from 'mysql2/promise';
+
+// export const pool = mysql.createPool({
+//   host: process.env.DB_HOST || '127.0.0.1',
+//   port: Number(process.env.DB_PORT || 3306),
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_NAME || 'sehat_vault',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   dateStrings: false
+// });
+
+// export async function connectDB() {
+//   const connection = await pool.getConnection();
+//   connection.release();
+//   console.log('MySQL connected');
+// }
+
+
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
@@ -6,6 +26,7 @@ export const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'sehat_vault',
+  ssl: process.env.DB_HOST && process.env.DB_HOST !== '127.0.0.1' ? { rejectUnauthorized: false } : false,
   waitForConnections: true,
   connectionLimit: 10,
   dateStrings: false
